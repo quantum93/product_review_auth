4599574f3a1360e9fdc5a30d7c5dafb082b56c1e
Tae Lee <taebumlee@gmail.com>
2019-08-09 11:34:48 -0700

    It is done for views of product and review class.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,6 +21,26 @@ class ReviewsController < ApplicationController
     render :show
   end
 
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
   # Other controller actions go here.
 
   private
