96c6187b1fcd693e11d6e2a48a92b1ec9a49da6d
Tae Lee <taebumlee@gmail.com>
2019-08-09 13:20:48 -0700

    It adds random 50 products with Faker.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food.dish,
+                  cost: Faker::Number.between(from: 5, to: 20),
+                  country_of_origin: Faker::Address.country)
+end
+
+p "Created #{Product.count} products"
